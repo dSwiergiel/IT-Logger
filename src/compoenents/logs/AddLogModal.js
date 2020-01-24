673626ac6db7f00e9accd249337650dd97d891ab
Deven Swiergiel <Deven.Swiergiel@its.ny.gov>
2020-01-24 08:56:09 -0500

    Finish ITLogger application

--- src/compoenents/logs/AddLogModal.js
+++ src/compoenents/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
@@ -57,9 +58,7 @@ const AddLogModal = ({ addLog }) => {
               <option value='' disabled>
                 Select Technician
               </option>
-              <option value='Alex Jones'>Alex Jones</option>
-              <option value='Lena Dunam'>Lena Dunam</option>
-              <option value='Bos Bagget'>Bos Bagget</option>
+              <TechSelectOptions></TechSelectOptions>
             </select>
           </div>
         </div>
